8f0dda73206a011148078070e3432e9e724a7e06
Saurabh Thakur <thakursaurabh1998@gmail.com>
2018-08-10 14:54:31 +0530

    cors

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post("/comic", (req, res) => {
   const params = {
